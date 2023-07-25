f0f5d6f5fed0afd6c6eaa30d8015d5ce99fc063e
Hassan Rasu <hassanrasu04@gmail.com>
2023-07-25 14:22:05 +0600

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function ($) {
+        $('.hero-slider').slick({
+            dots: false,
+            arrows: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: false,
+            autoplay: true,
+
+        });
+
+        
+
+        $(".mainmenu ul li:has(ul)").addClass("has-submenu");
+        $(".mainmenu ul li:has(ul)").addClass("small-submenu");
+        $(".mainmenu ul li ul").addClass("sub-menu");
+        $(".mainmenu ul.dropdown li").hover(function () {
+            $(this).addClass("hover");
+        }, function () {
+            $(this).removeClass("hover");
+        });
+
+        var $menu = $("#menu"),
+            $menulink = $("#menu-toggle"),
+            $menuTriggercont = $(".header-toggle"),
+            $searchToggle = $(".search_toggle"),
+            $menuTrigger = $(".has-submenu > a");
+        $menulink.click(function (e) {
+            $menulink.toggleClass("active");
+            $menu.toggleClass("active");
+            $menuTriggercont.toggleClass("active");
+        });
+
+        $searchToggle.click(function (e) {
+            $('.search').toggleClass("active");;
+        });
+
+        $menuTrigger.click(function (e) {
+            e.preventDefault();
+            var t = $(this);
+            t.toggleClass("active").next("ul").toggleClass("active")
+        });
+
+        $(".mainmenu ul li:has(ul)");
+
+
+
+
+
+    });
+
+
+
+}(jQuery));
+
+jarallax(document.querySelectorAll('.jarallax'), {
+    speed: 0.2
+});
